e6e5e71efc02bdc9c1541c8ad63656e5d9ddcc2d
zeynepyeniyapan <114344102+zeynepyeniyapan@users.noreply.github.com>
2024-08-14 17:31:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double sayi1, sayi2;
+string yapilmakIstenenIslem;
+Console.Write("1. sayıyı gir: ");
+sayi1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("2.sayıyı gir: ");
+sayi2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("yapmak istediğin işlemi gir: ");
+yapilmakIstenenIslem = Console.ReadLine();
+switch(yapilmakIstenenIslem)
+{
+    case "+":
+        Console.WriteLine("Sonuç: " + (sayi1 + sayi2));
+        break;
+    case "-":
+        Console.WriteLine("Sonuç: " +  (sayi1 - sayi2)); 
+        break;
+    case "*":
+        Console.WriteLine("Sonuç: " + (sayi1 * sayi2));
+        break;
+    case "/":
+        Console.WriteLine("Sonuç: " + (sayi1 / sayi2));
+        break;
+    default:
+        Console.WriteLine("yanlış bir tuşa bastın.");
+        break;
+}
+
+
